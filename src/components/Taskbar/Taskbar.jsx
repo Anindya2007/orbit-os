d049d51feece15abf9df76db9b1985ee02d0d40d
Anindya Sarkar <sanindya710@gmail.com>
2026-05-31 15:29:49 +0530

    Updated design of taskbar

--- src/components/Taskbar/Taskbar.jsx
+++ src/components/Taskbar/Taskbar.jsx
@@ -1,7 +1,11 @@
 import './Taskbar.css'
+import Clock from './Clock.jsx';
+import {Atom} from 'lucide-react';
 
 export default function taskbar(){
     return (<div id='Taskbar'>
-
+        <button id='orbit-button'>{<Atom size={35}/>}</button>
+        <div id='taskbar-content'></div>
+        <Clock/>
     </div>)
 }
